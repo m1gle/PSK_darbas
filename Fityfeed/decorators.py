f3d09535330e3f2d119fb371bad5cc0c3895e84b
m1gle <78843923+m1gle@users.noreply.github.com>
2023-09-11 21:39:46 +0300

    Add files via upload

---
+++ Fityfeed/decorators.py
@@ -0,0 +1,39 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect
+
+def unauthorized_user(view_func):
+    def wrapper_func(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home')
+        else:
+            return view_func(request, *args, **kwargs)
+    return wrapper_func
+
+def allowed_users(allowed_roles=[]):
+    def decorator(view_func):
+        def wrapper_func(request, *args, **kwargs):
+            group = None
+            if request.user.groups.exists():
+                group = request.user.groups.all()[0].name
+            print(f"User Group: {group}, Allowed Roles: {allowed_roles}")
+            if group in allowed_roles:
+                return view_func(request, *args, **kwargs)
+            else:
+                print(f"User Group: {group}, Allowed Roles: {allowed_roles}")
+                return HttpResponse("<h1>You are not allowed to access this page</h1>")
+        return wrapper_func
+    return decorator
+
+def admin_only(view_func):
+    def wrapper_func(request, *args, **kwargs):
+        group = None
+        if request.user.groups.exists():
+            group = request.user.groups.all()[0].name
+        if group == 'user':
+            return redirect('userPage')
+        elif group == 'admin':
+            return view_func(request, *args, **kwargs)
+        else:
+            print(f"User Group: {group}")
+            return HttpResponse("<h1>You are not allowed to access this page</h1>")
+    return wrapper_func
